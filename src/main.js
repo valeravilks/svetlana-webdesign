d0b1bffcef730f7d959080fbb882d15ba34f0641
valeravilks <valeravilks@gmail.com>
2020-03-16 21:30:34 +0300

    Add cursor basic

--- src/main.js
+++ src/main.js
@@ -160,5 +160,59 @@ if($('body').hasClass('to-index')){
     window.location.href = '/ru'
 }
 
+// Cursor
 
+let cursorHtml = `
+      <div class="cursor__ball cursor__ball--big ">
+        <svg height="30" width="30">
+          <circle cx="15" cy="15" r="12" stroke-width="0"></circle>
+        </svg>
+      </div>
+      
+      <div class="cursor__ball cursor__ball--small">
+        <svg height="10" width="10">
+          <circle cx="5" cy="5" r="4" stroke-width="0"></circle>
+        </svg>
+      </div>
+`;
+
+let nodeCursor = document.createElement('div');
+nodeCursor.classList.add('cursor');
+nodeCursor.innerHTML = cursorHtml;
+document.body.appendChild(nodeCursor);
+
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+    $hoverables[i].addEventListener('mouseenter', onMouseHover);
+    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e) {
+    gsap.to($bigBall, .8, {
+        x: e.pageX - 15,
+        y: e.clientY - 15
+    })
+    gsap.to($smallBall, .1, {
+        x: e.pageX - 5,
+        y: e.clientY - 10
+    })
+}
+
+// Hover an element
+function onMouseHover() {
+    gsap.to($bigBall, .3, {
+        scale: 4
+    })
+}
+function onMouseHoverOut() {
+    gsap.to($bigBall, .3, {
+        scale: 1
+    })
+}
 
