ce00d91682a19b16a6571190ba01be895289f1ae
valeravilks <valeravilks@gmail.com>
2020-03-18 19:08:27 +0300

    English home page added content

--- src/main.js
+++ src/main.js
@@ -296,11 +296,3 @@ $('.link').click( function(e){
 
 let pScroll = new ProgressScroll();
 
-$(document).bind( 'mousewheel', function (e) {
-    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100);
-    e.preventDefault();
-    e.stopPropagation();
-    $(document.body).stop().animate( {
-        scrollTop : nt
-    } , 500 , 'easeInOutCubic' );
-} )
